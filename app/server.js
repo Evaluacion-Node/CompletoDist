97884c01af551e1c92bd9451a3634560e5e26c05
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-06-08 14:27:05 -0500

    Probando dominio

--- app/server.js
+++ app/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import ruta from "./routes/index.js";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
+app.use(morgan("dev"))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 app.use("/",ruta);
 
 export default app;
